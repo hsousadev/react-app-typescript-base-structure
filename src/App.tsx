1886d6e5ff0943b0737e8ea79e1ac6359da55d27
justhenrique <washington.sousa01@fatec.sp.gov.br>
2021-04-10 17:20:40 -0300

    base-setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h2>React App (with Typescript) Base Setup</h2>
+      <p>ESLint + Prettier + EditorConfig + Pre-Commit Verification</p>
+      <strong>by Justhenrique</strong>
+    </>
+  );
+}
+
+export default App;
